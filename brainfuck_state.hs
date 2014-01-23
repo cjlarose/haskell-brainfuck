e0ab87d17c6b566fb12adb023a23167034238c32
Chris LaRose <cjlarose@gmail.com>
2014-01-22 18:17:56 -0700

    handle . and ,

--- brainfuck_state.hs
+++ brainfuck_state.hs
@@ -12,29 +12,41 @@ data BFState =
 
 -- read the instruction of the bfstate at its program counter, feed it to a fn
 -- that'll return a new BFState
-executeCommand :: BFState -> Char -> (Maybe Int, BFState)
-executeCommand s '>' = (Nothing, BFState {
+executeCommand :: BFState -> Char -> Int -> (Maybe Int, BFState)
+executeCommand s '>' _ = (Nothing, BFState {
     program = program s, 
     programCounter = programCounter s + 1, 
     tape = tape s, 
     dataPointer = dataPointer s + 1})
-executeCommand s '<' = (Nothing, BFState {
+executeCommand s '<' _ = (Nothing, BFState {
     program = program s, 
     programCounter = programCounter s + 1,
     tape = tape s, 
     dataPointer = dataPointer s - 1})
-executeCommand s '+' = (Nothing, BFState {
+executeCommand s '+' _ = (Nothing, BFState {
     program = program s, 
     programCounter = programCounter s + 1,
     tape = newTape, 
     dataPointer = dataPointer s})
     where newTape = tape s // [(dataPointer s, tape s ! dataPointer s + 1)]
-executeCommand s '-' = (Nothing, BFState {
+executeCommand s '-' _ = (Nothing, BFState {
     program = program s, 
     programCounter = programCounter s + 1,
     tape = newTape, 
     dataPointer = dataPointer s})
     where newTape = tape s // [(dataPointer s, tape s ! dataPointer s - 1)]
+executeCommand s '.' _ = (Just value, BFState {
+    program = program s, 
+    programCounter = programCounter s + 1,
+    tape = tape s, 
+    dataPointer = dataPointer s})
+    where value = tape s ! dataPointer s
+executeCommand s ',' c = (Nothing, BFState {
+    program = program s, 
+    programCounter = programCounter s + 1,
+    tape = newTape, 
+    dataPointer = dataPointer s})
+    where newTape = tape s // [(dataPointer s, c)]
 
 main = do
        program <- getContents
